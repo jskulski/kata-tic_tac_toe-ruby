ad914b6ab46063aacb23d01b5c726bccbe85e487
Jon Skulski <jskulski@gmail.com>
2014-12-20 23:37:52 -0500

    Can make move

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,2 +1,4 @@
 class TicTacToe
+	def move
+	end
 end
--- tic_tac_toe_spec.rb
+++ tic_tac_toe_spec.rb
@@ -7,6 +7,11 @@ describe "Tic Tac Toe" do
 		TicTacToe.new
 	end
 
+	it "game can make move" do
+		game = TicTacToe.new
+		game.move
+	end
+
 
 end
 
