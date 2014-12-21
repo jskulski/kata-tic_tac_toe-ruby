77b08a2720ff33466c0bb66ce43d1cf8fd0322b2
Jon Skulski <jskulski@gmail.com>
2014-12-20 23:33:16 -0500

    Initial commit

---
+++ Rakefile
@@ -0,0 +1,5 @@
+task :default => :spec
+
+task :spec do
+	sh "bundle exec rspec tic_tac_toe_spec.rb"
+end
---
+++ tic_tac_toe_spec.rb
@@ -0,0 +1,7 @@
+require 'rspec-given'
+require_relative 'spec_helper'
+
+describe "Tic Tac Toe" do
+end
+
+
