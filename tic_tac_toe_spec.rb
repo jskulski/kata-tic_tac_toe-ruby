2d06c17157f73820e9ccd8807926b6a4c29a33dd
Jon Skulski <jskulski@gmail.com>
2014-12-20 23:40:45 -0500

    Refactoring tests

--- tic_tac_toe_spec.rb
+++ tic_tac_toe_spec.rb
@@ -3,22 +3,19 @@ require_relative 'spec_helper'
 
 describe "Tic Tac Toe" do
 
+	Given(:game) { TicTacToe.new }
+
 	it "game can be created" do
-		TicTacToe.new
 	end
 
 	it "a move can be made" do
-		game = TicTacToe.new
 		game.move
 	end
 
 	it "can determine if there is a winner" do
-		game = TicTacToe.new
 		game.winner?
 	end
-
-
-
+	
 end
 
 
