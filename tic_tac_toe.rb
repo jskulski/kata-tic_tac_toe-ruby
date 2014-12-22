7551985867495ca92caf5b4b64a7706bf04c86d0
Jon Skulski <jskulski@gmail.com>
2014-12-22 10:58:41 -0500

    Detecting no winner

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,9 +1,21 @@
 class TicTacToe
+	def initialize
+		@board = [ 
+			['-', '-', '-'],
+			['-', '-', '-'],
+			['-', '-', '-']
+		]
+	end
+
 	def make_move(player_token, x_cooridinate, y_cooridinate)
-		@player_token = player_token
+		@board[x_cooridinate][y_cooridinate] = player_token
 	end
 
 	def winner?
-		@player_token
+		if @board[0][0] == @board[0][1] && @board[0][1] == @board[0][2]
+			@board[0][2]
+		else
+			false
+		end
 	end
 end
--- tic_tac_toe_spec.rb
+++ tic_tac_toe_spec.rb
@@ -23,6 +23,19 @@ describe "Tic Tac Toe" do
 			result = game.winner?
 			expect(result).to eq(player_token)
 		end
+
+		it "detects no winner if there is not winner in top row" do
+			player_one_token = 'X'
+			player_two_token = 'Y'
+
+			game.make_move(player_one_token, 0, 0)
+			game.make_move(player_two_token, 0, 1)
+			game.make_move(player_one_token, 0, 2)
+
+			result = game.winner?
+			expect(result).to eq(false)
+		end
+
 	end
 
 end
